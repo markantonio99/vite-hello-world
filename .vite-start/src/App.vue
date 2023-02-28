6b94b2565ea900dcec86a9d532293140e79388d4
“markantonio99” <markomakkia@gmail.com>
2023-02-28 17:03:36 +0100

    salv

---
+++ .vite-start/src/App.vue
@@ -0,0 +1,63 @@
+<script >
+
+import AppTitle from './components/AppTitle.vue'
+export default{
+  components: {
+    AppTitle
+  },
+  data(){
+    return{
+      message: 'ciao a tutti'
+    }
+  },
+  methods: {
+    viteHello(){
+
+      console.log('ciao Vite')
+
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <div><h1>{{message}}</h1></div>
+  <button @click="viteHello">ciao ciao</button>
+  
+  <AppTitle></AppTitle>
+
+</template>
+
+
+
+
+
+
+
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ .vite-start/src/components/AppTitle.vue
@@ -0,0 +1,42 @@
+<script >
+export default{
+ data(){
+   return{
+     message: 'La mia prima app con Vite'
+   }
+ }
+}
+</script>
+
+
+
+<template>
+    <div>
+        <h1 class="title_vue">{{ message }}</h1>
+    </div>
+</template>
+
+
+
+
+
+<style>
+.title_vue{
+    text-align: center;
+    background-color: green;
+    color: white;
+    padding: 15px;;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
